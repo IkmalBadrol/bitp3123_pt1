e71da73850d7c46f8acb43813ffb47f925d5afc0
IkmalBadrol <ikmalbadrol29@gmail.com>
2023-05-07 22:04:43 +0800

    Add more attributes to Location

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,6 +3,25 @@ package samples.demo;
 public class Location {
 	
 	private int Loc;
+	private String LocName;
+	private String LocName2;
+	
+	public String getLocName() {
+		return LocName;
+	}
+
+	public void setLocName(String locName) {
+		LocName = locName;
+	}
+
+	public String getLocName2() {
+		return LocName2;
+	}
+
+	public void setLocName2(String locName2) {
+		LocName2 = locName2;
+	}
+
 
 	public int getLoc() {
 		return Loc;
